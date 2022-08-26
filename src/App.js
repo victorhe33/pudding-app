065071a810cc6bee89720c39adefea38027f9147
Victor <94798862+victorhe33@users.noreply.github.com>
2022-08-26 14:50:35 -0400

    start with Vik

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+// import Clock from '.'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Clock/>
     </div>
   );
 }
+
+function Clock() {
+  return (
+    <div>
+      <h1>hello world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+
 
 export default App;
